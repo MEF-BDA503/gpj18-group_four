ea34e7eb6054e54b015fc4aea684ad37d82c9176
EmreKemerci <EmreKemerci@users.noreply.github.com>
2018-11-25 13:39:19 +0300

    .

--- sourceCode/termProjectEmre-jtable rds cevrim.R
+++ sourceCode/termProjectEmre-jtable rds cevrim.R
@@ -1,11 +1,4 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 library(tidyverse)
@@ -14,7 +7,7 @@ library(readxl)
 library(leaflet)
 
 
-#PATH <-"C:/Users/avs1400/Desktop/master/DataAnEssR_503/odev"
+
 PATH <-"C:/Users/emrek/Google Drive/BDA/503-EssentialsOfDataAnalytics/GitHub/gpj18-group_four/Raw Data"
 
 setwd(PATH)
@@ -40,6 +33,11 @@ getDataSet <- function(pPattern){
   return(resultDataset)
   
 }
+
+
+
+
+
 
 if (exists("import_quantity_dataset")){
   rm("import_quantity_dataset")    
@@ -79,6 +77,9 @@ import_value_dataset$Product_Code<- gsub("'", "",import_value_dataset$Product_Co
 export_quantity_dataset$Product_Code<- gsub("'", "",export_quantity_dataset$Product_Code)
 export_value_dataset$Product_Code<- gsub("'", "",export_value_dataset$Product_Code)
 
+
+
+
 import_jTable <- import_quantity_dataset %>% 
   mutate( Unit= coalesce(Unit1,Unit2 ,Unit3 , Unit4,Unit5)) %>%
   select( Country, Product_Code , Unit, Quantity_2013, Quantity_2014,Quantity_2015,Quantity_2016,Quantity_2017) %>% 
@@ -93,109 +94,63 @@ export_jTable <- export_quantity_dataset %>%
 export_jTable <- export_jTable %>% mutate(PcodeTwo=substr(Product_Code,1,2))
 export_jTable <- left_join(export_jTable,prdCode,by =c("PcodeTwo"="X__1") )
 
+
+
+
 export_jTable <- export_jTable %>% filter(!grepl("TOTAL", Product_Code))
 
 import_jTable <- import_jTable %>% filter(!grepl("TOTAL", Product_Code))
+
+
+
+
+
+q2013<-import_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2013) %>% mutate(year="2013") %>% rename("Quantity"=Quantity_2013)
+q2014<-import_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2014) %>% mutate(year="2014") %>% rename("Quantity"=Quantity_2014)
+q2015<-import_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2015) %>% mutate(year="2015") %>% rename("Quantity"=Quantity_2015)
+q2016<-import_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2016) %>% mutate(year="2016") %>% rename("Quantity"=Quantity_2016) 
+q2017<-import_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2017) %>% mutate(year="2017") %>% rename("Quantity"=Quantity_2017)
+
+importQuant <- bind_rows(q2013,q2014,q2015,q2016,q2017)
+
+v2013<-import_jTable %>% select(Country,Product_Code, Value_2013) %>% rename("Values"=Value_2013) %>% mutate(year="2013")
+v2014<-import_jTable %>% select(Country,Product_Code, Value_2014) %>% rename("Values"=Value_2014) %>% mutate(year="2014")
+v2015<-import_jTable %>% select(Country,Product_Code, Value_2015) %>% rename("Values"=Value_2015) %>% mutate(year="2015")
+v2016<-import_jTable %>% select(Country,Product_Code, Value_2016) %>% rename("Values"=Value_2016) %>% mutate(year="2016") 
+v2017<-import_jTable %>% select(Country,Product_Code, Value_2017) %>% rename("Values"=Value_2017) %>% mutate(year="2017")
+
+importValues <- bind_rows(v2013,v2014,v2015,v2016,v2017)
+
+import_jTableMELTED <- importQuant %>% left_join(importValues, by=c("Country","Product_Code","year"))
+
+
+
+q2013<-export_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2013) %>% mutate(year="2013") %>% rename("Quantity"=Quantity_2013)
+q2014<-export_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2014) %>% mutate(year="2014") %>% rename("Quantity"=Quantity_2014)
+q2015<-export_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2015) %>% mutate(year="2015") %>% rename("Quantity"=Quantity_2015)
+q2016<-export_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2016) %>% mutate(year="2016") %>% rename("Quantity"=Quantity_2016) 
+q2017<-export_jTable %>% select(Country, PcodeTwo, X__2 ,Product_Code, Product_Label, Unit, Quantity_2017) %>% mutate(year="2017") %>% rename("Quantity"=Quantity_2017)
+
+exportQuant <- bind_rows(q2013,q2014,q2015,q2016,q2017)
+
+v2013<-export_jTable %>% select(Country,Product_Code, Value_2013) %>% rename("Values"=Value_2013) %>% mutate(year="2013")
+v2014<-export_jTable %>% select(Country,Product_Code, Value_2014) %>% rename("Values"=Value_2014) %>% mutate(year="2014")
+v2015<-export_jTable %>% select(Country,Product_Code, Value_2015) %>% rename("Values"=Value_2015) %>% mutate(year="2015")
+v2016<-export_jTable %>% select(Country,Product_Code, Value_2016) %>% rename("Values"=Value_2016) %>% mutate(year="2016") 
+v2017<-export_jTable %>% select(Country,Product_Code, Value_2017) %>% rename("Values"=Value_2017) %>% mutate(year="2017")
+
+exportValues <- bind_rows(v2013,v2014,v2015,v2016,v2017)
+
+export_jTableMELTED <- exportQuant %>% left_join(exportValues, by=c("Country","Product_Code","year"))
+
+
+
 
 saveRDS(export_jTable,file="C:/Users/emrek/Google Drive/BDA/503-EssentialsOfDataAnalytics/GitHub/gpj18-group_four/export_jTable.rds")
 
 saveRDS(import_jTable,file="C:/Users/emrek/Google Drive/BDA/503-EssentialsOfDataAnalytics/GitHub/gpj18-group_four/import_jTable.rds")
 
+saveRDS(export_jTableMELTED,file="C:/Users/emrek/Google Drive/BDA/503-EssentialsOfDataAnalytics/GitHub/gpj18-group_four/export_jTableMELTED.rds")
 
-
-
-countries <- 
-  import_jTable %>% 
-  distinct(Country) %>% 
-  unlist(.)
-names(countries) <- NULL
-
-products <- 
-  import_jTable %>% 
-  distinct(Product_Label) %>% 
-  unlist(.)
-names(products) <- NULL
-
-
-rm("importValues")
-rm("importQuant")
-q2013<-import_jTable %>% select(Country,Product_Code, Product_Label, Quantity_2013) %>% rename("Quantity"=Quantity_2013) %>% mutate(year="2013")
-q2014<-import_jTable %>% select(Country,Product_Code, Product_Label, Quantity_2014) %>% rename("Quantity"=Quantity_2014) %>% mutate(year="2014")
-q2015<-import_jTable %>% select(Country,Product_Code, Product_Label, Quantity_2015) %>% rename("Quantity"=Quantity_2015) %>% mutate(year="2015")
-q2016<-import_jTable %>% select(Country,Product_Code, Product_Label, Quantity_2016) %>% rename("Quantity"=Quantity_2016) %>% mutate(year="2016") 
-q2017<-import_jTable %>% select(Country,Product_Code, Product_Label, Quantity_2017) %>% rename("Quantity"=Quantity_2017) %>% mutate(year="2017")
-
-importQuant <- bind_rows(q2013,q2014,q2015,q2016,q2017)
-
-v2013<-import_jTable %>% select(Country,Product_Code, Product_Label, Value_2013) %>% rename("Values"=Value_2013) %>% mutate(year="2013")
-v2014<-import_jTable %>% select(Country,Product_Code, Product_Label, Value_2014) %>% rename("Values"=Value_2014) %>% mutate(year="2014")
-v2015<-import_jTable %>% select(Country,Product_Code, Product_Label, Value_2015) %>% rename("Values"=Value_2015) %>% mutate(year="2015")
-v2016<-import_jTable %>% select(Country,Product_Code, Product_Label, Value_2016) %>% rename("Values"=Value_2016) %>% mutate(year="2016") 
-v2017<-import_jTable %>% select(Country,Product_Code, Product_Label, Value_2017) %>% rename("Values"=Value_2017) %>% mutate(year="2017")
-
-importValues <- bind_rows(v2013,v2014,v2015,v2016,v2017)
-
-#productCodeNonZero <- 
-#  importValues %>% filter(Values>0) %>%
-#  distinct(Product_Code,Product_Label) %>% 
-#  unlist(.)
-#names(productCodeNonZero) <- NULL
-#rm("productCodeNonZero")
-#productCodeNonZero <- 
-#  import_jTable %>% filter(Value_2013>0 | Value_2014>0 | Value_2015>0 | Value_2016>0 | Value_2017>0) %>%
-#  gather(key = "Product_Code",value="Product_Label") 
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Import Analysis to Turkey"),
-   
-   #sliderInput("years",
-  #                   "Years",
-  #                   min = 2013,
-  #                   max = 2017,
-  #                   value = c(2014,2016),sep = ""),'''   
-  
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput(inputId =  "country", label = "Country:", choices = c("All", countries)),          
-         selectInput(inputId =  "products", label = "Products:", choices = c("All", products)) 
-        
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      #x    <- faithful[, 2] 
-      #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-     
-#     modified_data <- importValues %>% filter(year >= input$years[1] & year <= input$years[2] )
-     modified_data <- importValues #%>% filter(year >= input$years[1] & year <= input$years[2] )     
-     if(input$country != "All"){
-       modified_data <- importValues %>% filter(Country==input$country)
-     }
-     if(input$products != "All"){
-       modified_data <- modified_data %>% filter(Product_Label==input$products)
-     }
-     ggplot(modified_data,aes(x=year,y=Values)) + geom_bar(stat = "identity")
-     
-   })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+saveRDS(import_jTableMELTED,file="C:/Users/emrek/Google Drive/BDA/503-EssentialsOfDataAnalytics/GitHub/gpj18-group_four/import_jTableMELTED.rds")
 
