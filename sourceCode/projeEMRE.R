547d541876aa8525b4b9b1f7d01ffd578e2ba269
EmreKemerci <EmreKemerci@users.noreply.github.com>
2018-11-29 23:23:20 +0300

    Update projeEMRE.R

--- sourceCode/projeEMRE.R
+++ sourceCode/projeEMRE.R
@@ -27,33 +27,10 @@ export_jTableMELTED <- export_jTableMELTED %>% mutate(Trade="Export")
 import_jTableMELTED <- import_jTableMELTED %>% mutate(Trade="Import")
 
 
+table <- export_jTableMELTED %>%
+  group_by(Country, year) %>%
+  summarise(Trade_Value=sum(Values, na.rm=TRUE))
 
-library(scales)
-theme_set(theme_classic())
 
-table <- export_jTable %>% select(Country,Quantity_2013,Quantity_2014,Quantity_2015,Quantity_2016,Quantity_2017,Value_2013,Value_2014,Value_2015,Value_2016,Value_2017) %>% group_by(Country)
+table$change_perc <- c(0, diff(table$Trade_Value)/table$Trade_Value[-length(table$Trade_Value)])
 
-df$class <- ifelse((df$`1957` - df$`1952`) < 0, "red", "green")
-
-# Plot
-p <- ggplot(df) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
-  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
-  geom_vline(xintercept=2, linetype="dashed", size=.1) +
-  scale_color_manual(labels = c("Up", "Down"), 
-                     values = c("green"="#00ba38", "red"="#f8766d")) +  # color of lines
-  labs(x="", y="Mean GdpPerCap") +  # Axis labels
-  xlim(.5, 2.5) + ylim(0,(1.1*(max(df$`1952`, df$`1957`))))  # X and Y axis limits
-
-# Add texts
-p <- p + geom_text(label=left_label, y=df$`1952`, x=rep(1, NROW(df)), hjust=1.1, size=3.5)
-p <- p + geom_text(label=right_label, y=df$`1957`, x=rep(2, NROW(df)), hjust=-0.1, size=3.5)
-p <- p + geom_text(label="Time 1", x=1, y=1.1*(max(df$`1952`, df$`1957`)), hjust=1.2, size=5)  # title
-p <- p + geom_text(label="Time 2", x=2, y=1.1*(max(df$`1952`, df$`1957`)), hjust=-0.1, size=5)  # title
-
-# Minify theme
-p + theme(panel.background = element_blank(), 
-          panel.grid = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text.x = element_blank(),
-          panel.border = element_blank(),
-          plot.margin = unit(c(1,2,1,2), "cm"))
