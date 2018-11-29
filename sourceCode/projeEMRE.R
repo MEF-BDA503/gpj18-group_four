56c141ede7967222df5999d0bcc09d6c15f8beac
EmreKemerci <EmreKemerci@users.noreply.github.com>
2018-11-29 21:46:15 +0300

    .

--- sourceCode/projeEMRE.R
+++ sourceCode/projeEMRE.R
@@ -1,67 +1,59 @@
+library(shiny)
 library(tidyverse)
-library(readxl)
 library(dplyr)
-
-PATH <-"C:\Users\emrek\Google Drive\BDA\503-EssentialsOfDataAnalytics\GitHub\gpj18-group_four\Raw Data"
-setwd(PATH)
-
-
-getDataSet <- function(pPattern){
-  nm <- list.files(path = PATH, pattern = pPattern)  
-  if (exists("resultDataset")){
-    rm("resultDataset")
-  }
-  for (file in nm){
-    if (!exists("resultDataset")){
-      resultDataset <- read_excel(file, col_names=FALSE, skip=1)
-    }
-    else{
-      temp_dataset <-read_excel(file, col_names=FALSE, skip=1)
-      resultDataset<-rbind(resultDataset, temp_dataset)
-      rm(temp_dataset)
-    }
-    
-  }
-  return(resultDataset)
-  
-}
-
-if (exists("import_quantity_dataset")){
-  rm("import_quantity_dataset")    
-}  
-if (exists("import_value_dataset")){
-  rm("import_value_dataset")    
-}  
-if (exists("import_jTable")){
-  rm("import_jTable")
-}
-if (exists("export_quantity_dataset")){
-  rm("export_quantity_dataset")    
-}  
-if (exists("export_value_dataset")){
-  rm("export_value_dataset")    
-}  
-if (exists("export_jTable")){
-  rm("export_jTable")
-}
-
-import_quantity_dataset <- getDataSet('*.mport.*uant.*')
-import_value_dataset <- getDataSet('*.mport.*alu.*')
-export_quantity_dataset <- getDataSet('*.xport.*uant.*')
-export_value_dataset <- getDataSet('*.xport.*alu.*')
-
-colnames(import_quantity_dataset) <- c("Country","Product_Code","Product_Label","Quantity_2013","Unit1","Quantity_2014","Unit2","Quantity_2015","Unit3","Quantity_2016","Unit4","Quantity_2017","Unit5")
-colnames(import_value_dataset) <- c("Country","Product_Code","Product_Label","Value_2013","Value_2014","Value_2015","Value_2016","Value_2017")
-colnames(export_quantity_dataset) <- c("Country","Product_Code","Product_Label","Quantity_2013","Unit1","Quantity_2014","Unit2","Quantity_2015","Unit3","Quantity_2016","Unit4","Quantity_2017","Unit5")
-colnames(export_value_dataset) <- c("Country","Product_Code","Product_Label","Value_2013","Value_2014","Value_2015","Value_2016","Value_2017")
+library(readxl)
+library(leaflet)
+library(reshape2)
 
 
-import_jTable <- import_quantity_dataset %>% 
-  mutate( Unit= coalesce(Unit1,Unit2 ,Unit3 , Unit4,Unit5)) %>%
-  select( Country, Product_Code , Unit, Quantity_2013, Quantity_2014,Quantity_2015,Quantity_2016,Quantity_2017) %>% 
-  full_join(import_value_dataset,by = c("Country","Product_Code"))  
+githubURL_export <- ("https://github.com/MEF-BDA503/gpj18-group_four/blob/master/export_jTable.rds?raw=true")
+githubURL_import <- ("https://github.com/MEF-BDA503/gpj18-group_four/blob/master/import_jTable.rds?raw=true")
+githubURL_exportMELTED <- ("https://github.com/MEF-BDA503/gpj18-group_four/blob/master/export_jTableMELTED.rds?raw=true")
+githubURL_importMELTED <- ("https://github.com/MEF-BDA503/gpj18-group_four/blob/master/import_jTableMELTED.rds?raw=true")
 
-export_jTable <- export_quantity_dataset %>% 
-  mutate( Unit= coalesce(Unit1,Unit2 ,Unit3 , Unit4,Unit5)) %>%
-  select( Country, Product_Code , Unit, Quantity_2013, Quantity_2014,Quantity_2015,Quantity_2016,Quantity_2017) %>% 
-  full_join(export_value_dataset,by = c("Country","Product_Code"))  
+export_jTable<- readRDS(url(githubURL_export))
+import_jTable<- readRDS(url(githubURL_import))
+export_jTableMELTED<- readRDS(url(githubURL_exportMELTED))
+import_jTableMELTED<- readRDS(url(githubURL_importMELTED))
+
+rm(githubURL_export)
+rm(githubURL_import)
+rm(githubURL_exportMELTED)
+rm(githubURL_importMELTED)
+
+
+export_jTableMELTED <- export_jTableMELTED %>% mutate(Trade="Export")
+
+import_jTableMELTED <- import_jTableMELTED %>% mutate(Trade="Import")
+
+
+
+library(scales)
+theme_set(theme_classic())
+
+table <- export_jTable %>% select(Country,Quantity_2013,Quantity_2014,Quantity_2015,Quantity_2016,Quantity_2017,Value_2013,Value_2014,Value_2015,Value_2016,Value_2017) %>% group_by(Country)
+
+df$class <- ifelse((df$`1957` - df$`1952`) < 0, "red", "green")
+
+# Plot
+p <- ggplot(df) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
+  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
+  geom_vline(xintercept=2, linetype="dashed", size=.1) +
+  scale_color_manual(labels = c("Up", "Down"), 
+                     values = c("green"="#00ba38", "red"="#f8766d")) +  # color of lines
+  labs(x="", y="Mean GdpPerCap") +  # Axis labels
+  xlim(.5, 2.5) + ylim(0,(1.1*(max(df$`1952`, df$`1957`))))  # X and Y axis limits
+
+# Add texts
+p <- p + geom_text(label=left_label, y=df$`1952`, x=rep(1, NROW(df)), hjust=1.1, size=3.5)
+p <- p + geom_text(label=right_label, y=df$`1957`, x=rep(2, NROW(df)), hjust=-0.1, size=3.5)
+p <- p + geom_text(label="Time 1", x=1, y=1.1*(max(df$`1952`, df$`1957`)), hjust=1.2, size=5)  # title
+p <- p + geom_text(label="Time 2", x=2, y=1.1*(max(df$`1952`, df$`1957`)), hjust=-0.1, size=5)  # title
+
+# Minify theme
+p + theme(panel.background = element_blank(), 
+          panel.grid = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text.x = element_blank(),
+          panel.border = element_blank(),
+          plot.margin = unit(c(1,2,1,2), "cm"))
